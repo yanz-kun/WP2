c93ed6531b956dda59ded905b70c8fd9340d18d7
yanz-kun <56082058+yanz-kun@users.noreply.github.com>
2020-10-26 20:30:47 +0700

    Add files via upload

---
+++ praktikum/praktikum1/Perkenalan.php
@@ -0,0 +1,11 @@
+<?php
+class Perkenalan extends CI_Controller
+{
+	public function index()
+	{
+		echo "<h1>Perkenalkan<h1>";
+		echo "<h2>Yanuar Ardani N<br>
+			Saya tinggal di Tegal<br>
+			Olahraga yang saya sukai adalah Futsal<h2>";
+	}
+}
---
+++ praktikum/praktikum2/view-latihan1.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <title>Latihan 1</title>
+<head>
+
+<body>
+    Halo kawan .. Yuk kita belajar web programming ... !!! <br>
+    Nilai 1 = <?= $nilai1; ?> <br>
+    Nilai 2 = <?= $nilai2; ?> <br>
+    ini hasil dari pemodelan dengan metode penjumlahan yaitu <?= $nilai1." + ".$nilai2." = ".$hasil; ?>
+</body>
+</html>
